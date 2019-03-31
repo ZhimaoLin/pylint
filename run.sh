bca8da7d75834d7f6bee54c68390d2a3a6c7bca3
Zhimao Lin <zhimao@ualberta.ca>
2019-03-31 12:29:35 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+# Install Pylint
+pip3 install .
+cd bin
+
+echo -e "${GREEN}===================================================${NOCOLOR}"
+
 if [ "$1" = "" ]
 then
-    echo "Please put python files that you want to analyze in the /please_put_test_python_file_here folder"
-    echo "Please give a python file name that you want to analyze as argument 1"
-    echo "try to run:"
-    echo "./run.sh analyze_this_python_file.py"
+    for file in "../please_put_test_python_file_here"/*
+    do
+        python3 pylint $file
+    done
 else
-    pip3 install .
-    cd bin
     python3 pylint ../please_put_test_python_file_here/$1
 fi
 
