68fd665dbf67fda606b5b61fbfd15aa43b6756cf
Zhimao Lin <zhimao@ualberta.ca>
2019-03-31 17:44:56 -0600

    Update test7_4_bad.py

--- please_put_test_python_file_here/test7_4_bad.py
+++ please_put_test_python_file_here/test7_4_bad.py
@@ -3,15 +3,18 @@ def many_arg_func():
     apple = 1
     orange = 2
     banana = 3
-    banana = apple + orange
-    print(banana)
-    apple = banana - orange
-    print(apple)
-    orange = banana - apple
-    print(orange)
-    banana = apple + orange
-    print(banana)
-    apple = banana - orange
-    print(apple)
-    orange = banana - apple
-    print(orange)
+    i = 100
+    if i > 2:
+        banana = apple + orange
+        print(banana)
+        apple = banana - orange
+        print(apple)
+        orange = banana - apple
+        print(orange)
+    else:
+        banana = apple + orange
+        print(banana)
+        apple = banana - orange
+        print(apple)
+        orange = banana - apple
+        print(orange)
