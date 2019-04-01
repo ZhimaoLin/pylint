bac4e45feecdddb4bb18e5d0e33b9324f9554599
wdong2 <wdong2@ualberta.ca>
2019-03-31 20:06:07 -0600

    test 6.2

---
+++ please_put_test_python_file_here/test6_2_bad.py
@@ -0,0 +1,14 @@
+#6.1 Does every literal (other than 0, 1, 2, -1, 0.0 and '') appear exactly once?
+def print1000():
+    print(1000)
+    apple = 10
+    orange = 20
+    banana = 30
+    return apple, orange, banana
+    
+def main():
+    print("this is main!")
+    print("this is test")
+    print1000()
+    
+main()
