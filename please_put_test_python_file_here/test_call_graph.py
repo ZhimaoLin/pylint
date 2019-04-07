0403811fffe557ee1591a05910a2355c350c18ea
Yi Zhang <yi16@ualberta.ca>
2019-04-06 19:13:24 -0600

    test file

--- please_put_test_python_file_here/test_call_graph.py
+++ please_put_test_python_file_here/test_call_graph.py
@@ -8,18 +8,57 @@ def f3():
     f2()
 def main():
     a = 3
-
-    if a > 2:
-        print()
+    try:
+        f1()
+        if True:
+            if_in_try()
+        else:
+            else_in_try()
+        for i in range(0, 3):
+            for_in_try()
+    except ZeroDivisionError:
+        f2()
+        if True:
+            if_in_except()
+        else:
+            else_in_except()
+        for i in range(0, 3):
+            for_in_except()
+    except:
+        f2()
+        f1()
+    finally:
+        if True:
+            if_in_finally()
+        else:
+            else_in_finally()
+        for i in range(0, 3):
+            for_in_finally()
+    f3()
+    if a < 2:
+        f1()
+        if True:
+            f2()
+            while 1:
+                function()
+        try:
+            try_in_if()
+        except:
+            except_in_if()
+        finally:
+            finally_in_if()
     else:
         f1()
-    
-    for i in range(1, 10):
-        print(i)
-
-    while a > 2:
-        print(1)
-        a -= 1
-
+        for i in range(0, 1):
+            for_in_else()
+        try:
+            try_in_else()
+        except:
+            except_in_else()
+        finally:
+            finally_in_else()
+            while True:
+                while_in_finally()
 
     # f3()
+main()
