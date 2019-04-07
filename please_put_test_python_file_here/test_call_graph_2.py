d9281bf55fe52cc313b8b3399e478e08f30cb3f7
Zhimao Lin <zhimao@ualberta.ca>
2019-04-06 19:25:06 -0600

    test case 2

---
+++ please_put_test_python_file_here/test_call_graph_2.py
@@ -0,0 +1,44 @@
+def f1():
+    print("this is f1")
+    return 5
+def f2():
+    print("this is f2")
+def f3():
+    print("this is f3")
+    f1()
+    f2()
+def main():
+    a = 3
+
+    b = 4
+
+    a = b
+
+    a = f1()
+
+    a += f2()
+
+    a, b = f1()
+
+    f3()
+
+    if a > 2:
+        print()
+    elif True:
+        print()
+    else:
+        f3()
+    
+    for i in range(1, 10):
+        print(i)
+        while a > 2:
+            # a -= 1
+            f1()
+
+    
+    try:
+        print()
+    except:
+        f3()
+    finally:
+        print()
